2676fd5d8d9474db045ca5a448140e0aa7ef04b8
Taylor Phillips <taylorscottphillips@gmail.com>
2021-11-30 18:52:04 -0600

    Add spinner component

---
+++ src/components/LoadSpinner/LoadSpinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LoadSpinner.css'
+
+function LoadSpinner() {
+  return (
+    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default LoadSpinner
